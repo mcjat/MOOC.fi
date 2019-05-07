727e29febb030f045d168eb441fab405e3302270
Ian <49131991+mcjat@users.noreply.github.com>
2019-05-07 16:45:19 -0700

    Create Main.java

---
+++ Week 9/Exercise 23: WordInspection/src/wordinspection/Main.java
@@ -0,0 +1,10 @@
+package wordinspection;
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        // test your code here
+        File file = new File("src/shortList.txt");
+        // all words are in file src/wordList.txt
+    }
+}
