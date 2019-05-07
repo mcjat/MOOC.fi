b2c9061f9bfee947add5cac0512a90ebe64c4a1a
Ian <49131991+mcjat@users.noreply.github.com>
2019-05-07 16:48:54 -0700

    Create DuplicateRemover.java

---
+++ Week 9/Exercise 25: DuplicateRemover/src/tools/DuplicateRemover.java
@@ -0,0 +1,9 @@
+package tools;
+import java.util.Set;
+
+public interface DuplicateRemover {
+    void add(String characterString);
+    int getNumberOfDetectedDuplicates();
+    Set<String> getUniqueCharacterStrings();
+    void empty();
+}
