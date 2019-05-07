8f0aaa0d8e29497603cc03289af3019d2b3a1d1a
Ian <49131991+mcjat@users.noreply.github.com>
2019-05-07 16:24:15 -0700

    Create TextUserInterface.java

---
+++ Exercise 17: FirstPackages/src/mooc/ui/TextUserInterface.java
@@ -0,0 +1,8 @@
+package mooc.ui;
+public class TextUserInterface implements UserInterface{
+    public TextUserInterface(){
+    }
+    public void update(){
+        System.out.println("Updating the user interface");
+    }
+}
