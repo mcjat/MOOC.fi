f913df7aaac509334eb88bceddeaaabb879162b9
Ian <49131991+mcjat@users.noreply.github.com>
2019-05-07 17:56:22 -0700

    Update and rename Exercise 03: Calculator.java to Week 07/Exercise 03: Calculator/Main.java

---
+++ Week 07/Exercise 03: Calculator/Main.java
@@ -0,0 +1,14 @@
+/*  Simple calculator which can sum, subtract, or multiply two integers
+*   based on user inputs.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        // Write some code here to test your program
+
+        // Once you have done the class which implements the actual application logic,
+        // all you need to write here is:
+        Calculator calculator = new Calculator();
+        calculator.start();
+    }
+}
