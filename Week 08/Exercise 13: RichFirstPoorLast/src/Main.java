c51f9afd884f2d498c5cb00fe1baa3f866ff12c1
Ian <49131991+mcjat@users.noreply.github.com>
2019-05-07 18:23:58 -0700

    Update and rename Exercise 13: RichFirstPoorLast.java to Week 08/Exercise 13: RichFirstPoorLast/src/Main.java

--- Week 08/Exercise 13: RichFirstPoorLast/src/Main.java
+++ Week 08/Exercise 13: RichFirstPoorLast/src/Main.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Collections;
 
 public class Main {
-
     public static void main(String[] args) {
         List<Person> people = new ArrayList<Person>();
         people.add(new Person("Matti", 150000));
@@ -14,7 +13,6 @@ public class Main {
         people.add(new Person("Arto", 10));
         people.add(new Person("Merja", 500));
         people.add(new Person("Pertti", 80));
-
         System.out.println(people);
 
         /*
@@ -22,38 +20,5 @@ public class Main {
          */
         Collections.sort(people);
         System.out.println(people);
-
     }
-}
-
-
-public class Person implements Comparable<Person> {    
-
-    private int salary;
-    private String name;
-
-    public Person(String name, int salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-
-    @Override
-    public String toString() {
-        return name + " " + salary;
-    }    
-
-    @Override
-    public int compareTo(Person person){
-        return person.getSalary() - this.salary;
-    }
-
 }
