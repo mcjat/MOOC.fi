b4b76a9fcbddb4023ce544a1b6d20e518c3fce64
Ian <49131991+mcjat@users.noreply.github.com>
2019-05-07 17:13:37 -0700

    Create Teacher.java

---
+++ Week 10/Exercise 29: PersonAndTheirHeirs/src/people/Teacher.java
@@ -0,0 +1,19 @@
+package people;
+
+public class Teacher extends Person {    
+    private int salary;
+    
+    public Teacher(String name, String address, int salary) {
+        super(name, address);
+        this.salary = salary;
+    }
+    
+    public int salary(){
+        return this.salary;
+    }
+    
+    @Override
+    public String toString(){
+        return super.toString() + "\n  salary " + this.salary() + " euros/month";
+    }
+}
