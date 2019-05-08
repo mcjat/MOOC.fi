dddf527d9ba55f0c0757b46104bdced134e02a09
Ian <49131991+mcjat@users.noreply.github.com>
2019-05-07 18:16:37 -0700

    Update and rename Exercise 10: NationalService.java to Week 08/Exercise 10: NationalService/src/Main.java

--- Week 08/Exercise 10: NationalService/src/Main.java
+++ Week 08/Exercise 10: NationalService/src/Main.java
@@ -1,6 +1,8 @@
 //exercise for understanding basics of interfaces
+
 public class Main {
     public static void main(String[] args) {
+        // Test your code here!
         MilitaryService SgtJacob = new MilitaryService(1200);
         CivilService AdminJacob = new CivilService();
         
@@ -26,51 +28,6 @@ public class Main {
                 + " days of military service and " + AdminJacob.getDaysLeft()
                 + " days of civil sercice remaining.");
         
-      
+        
     }
-}
-
-
-public interface NationalService {
-    int getDaysLeft();
-    void work();
-}
-
-public class CivilService implements NationalService {
-    private int daysLeft;
-    
-    public CivilService(){
-        this.daysLeft = 362;
-    }
-    public int getDaysLeft(){
-        return this.daysLeft;
-    }
-    
-    public void work(){
-        if(this.daysLeft !=0){
-            this.daysLeft -= 1;
-        }
-    }
-    
-    
-}
-
-
-public class MilitaryService implements NationalService{
-    private int daysLeft;
-    
-    public MilitaryService(int daysLeft){
-        this.daysLeft = daysLeft;
-    }
-    
-    public int getDaysLeft(){
-        return this.daysLeft;
-    }
-    
-    public void work(){
-        if(this.daysLeft != 0){
-            this.daysLeft -= 1;
-        }
-    }
-    
 }
