fd2cebc2ff7d7b60af3a0546862cc876c54caacd
Ian <49131991+mcjat@users.noreply.github.com>
2019-05-07 18:31:32 -0700

    Update and rename Exercise 14: StudentsSortedByName.java to Week 08/Exercise 14: StudentsSortedByName/src/Main.java

---
+++ Week 08/Exercise 14: StudentsSortedByName/src/Main.java
@@ -0,0 +1,9 @@
+//exercise to show how Strings may be sorted using compareTo()
+
+public class Main {
+    public static void main(String[] args){
+        Student first = new Student("jamo");
+        Student second = new Student("jamo1");
+        System.out.println(first.compareTo(second));
+    }
+}
